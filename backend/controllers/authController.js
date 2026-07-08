f11f0c911405873e0c405f6b5fddc34221409eca
Aadi <aadijain.ltd97@gmail.com>
2026-07-08 07:17:48 +0530

    feat(auth): impliment JWT-based login authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 async function registerUser(req, res) {
   try {
@@ -22,6 +23,49 @@ async function registerUser(req, res) {
   }
 }
 
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      })
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password",
+      })
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id 
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    )
+
+    res.json({
+      message: "Login successful", 
+      token,
+    })
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).json({
+      message: "Login failed",
+    })
+  }
+}
+
 module.exports = {
   registerUser,
+  loginUser,
 }
